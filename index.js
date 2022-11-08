a8e27240203b31a0aa4cdbd1b59088f7ccaa9879
Rakib Linux <rakibulinux@gmail.com>
2022-11-08 09:39:30 +0600

    services limit added

---
+++ index.js
@@ -0,0 +1,65 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const JwtAuth = require("./jwtAuth");
+
+// Mongo DB Connections
+const client = new MongoClient(process.env.MONGO_DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+// Middleware Connections
+app.use(cors());
+app.use(express.json());
+
+// Main Route
+app.get("/", (req, res) => {
+  res.send("API Server is Running");
+});
+
+async function run() {
+  try {
+    const serviceCollection = client.db("serviceReview").collection("services");
+    const reviewCollection = client.db("serviceReview").collection("reviews");
+
+    // JWT Post Method
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      console.log(user.email);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
+    });
+
+    // Limit to 3 services
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+
+    //Get all services
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.post("/services", async (req, res) => {});
+  } finally {
+  }
+}
+
+run().catch((err) => console.error(err));
+
+// Connection
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log("App running in port: " + PORT);
+});
